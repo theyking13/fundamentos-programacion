a91e75a816c9dd0f5c546b9d205c77362cc6451c
theyking13hub <reinaldohormazabal243@gmail.com>
2025-01-07 21:30:25 -0300

    ejercicios varios if else

--- test.py
+++ test.py
@@ -1,1 +1,105 @@
-print("hola mundo")
+# nombre="reinaldo hormazabal"
+# edad="22"
+
+# print("Mi nombre es",nombre, "Mi edad es", edad)
+# num1=0
+# num2=0
+
+# print("ingrese un numero")
+# num1=int(input())
+# print("ingrese un numero")
+# num2=int(input())
+
+# print(num1+num2)
+
+# print("ingrese un numero")
+# num1=int(input())
+# if num1>0:
+#     print("el numero ingresado es positivo")
+# else:
+#     print("el numero es negativo o cero")
+
+# print("ingrese un numero")
+# num1=int(input())
+
+# if num1>18:
+#     print("es mayor de edad")
+# else:
+#     print("no es mayor de edad")
+
+# num1=0
+# num2=0
+# num3=0
+
+# print("ingrese el primer numero")
+# num1=int(input())
+
+# print("ingrese el segundo numero")
+# num2=int(input())
+
+# print("ingrese el tercer numero")
+# num3=int(input())
+# print((num1+num2+num3)/3)
+
+# parque nacional que tiene los sigientes valores
+
+# menor de 12$500
+# entre 12 y 18 $1000
+# entre 19 y 64 $2000
+# mas de 65 $700
+
+# bonus pregunte cuantas entradas son 
+
+# edad=0
+# entradas=0
+
+# print("bienvenidos al parque nacional")
+
+# print("cual es su edad")
+
+# edad=int(input("ingrese su edad"))
+# if edad<12:
+#     print("paga $500")
+# elif edad>=13 and edad<=18:
+#     print("paga $1000")
+# elif edad>=19 and edad<=64:
+#     print("paga $2000")
+# elif edad>=65:
+#     print("paga $700")
+
+# # entradas=0
+
+# print("cuantas entradas desea")
+# print("ingrese el total de entradas")
+# total_entradas=int(input("ingrese el total de entradas"))
+# print(f"el total de entradas es ")
+
+# edad = int(input("¿Cuál es su edad? "))  
+
+
+# if edad < 12:
+#     precio = 500
+#     print("Paga $500")
+# elif edad >= 13 and edad <= 18:
+#     precio = 1000
+#     print("Paga $1000")
+# elif edad >= 19 and edad <= 64:
+#     precio = 2000
+#     print("Paga $2000")
+# elif edad >= 65:
+#     precio = 700
+#     print("Paga $700")
+
+
+# print("¿Cuántas entradas desea?")
+# total_entradas = int(input("Ingrese el total de entradas: "))
+
+
+# total_a_pagar = precio * total_entradas
+# print("El total a pagar es:",total_a_pagar)
+
+
+op1="si"
+op2="no"
+numero=0
+descuento=0
